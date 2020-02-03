942e21a53ff0fa06064b7434e8c53a51bfecba4f
korzhovaolga <59731944+korzhovaolga@users.noreply.github.com>
2020-02-03 14:43:49 +0200

    Update js.js
    изменение стилей

--- javas/js.js
+++ javas/js.js
@@ -7,3 +7,29 @@ $(".navbar a").click(function(){
 		scrollTop:$("#" + $(this)
 			.data('value')).offset().top}, 1000) 
 })
+
+var head = document.getElementById('main_img');
+
+function changeBgImage(){
+    head.style.backgroundImage="url('https://cdn.pixabay.com/photo/2019/11/26/18/44/input-4655216_960_720.jpg')";
+}
+
+changeBgImage();
+
+var sect = document.getElementById('sect');
+sect.parentNode.removeChild(sect);
+
+var navbarColor=document.getElementById('navbarColor');
+navbarColor.style.backgroundColor = "rgba(41, 90, 250, 1)";
+
+var paragrafColor=document.getElementById('paragrafColor');
+paragrafColor.style.color="white";
+
+var portfolio=document.getElementById('portfolio');
+portfolio.style.backgroundColor="rgba(41, 90, 250, 1)";
+
+var blogImg=document.getElementById('blogImg');
+blogImg.style.backgroundImage="url('https://cdn.pixabay.com/photo/2017/06/23/11/41/lights-2434376_960_720.jpg')";
+
+var teamImg=document.getElementById('teamImg');
+teamImg.style.backgroundImage="url('https://cdn.pixabay.com/photo/2016/11/19/16/52/architecture-1840297_960_720.jpg')";
